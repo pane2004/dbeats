3967d7bebd2ba745dfcc59f23dc313795ce8844e
pane2004 <alexlu2000clash@gmail.com>
2023-09-24 05:50:01 -0400

    changes to ipfs storage upload

--- NFT-creation/scripts/mint-nft.mjs
+++ NFT-creation/scripts/mint-nft.mjs
@@ -1,20 +1,21 @@
-const META_DATA_URL = "ipfs://bafyreibue5zps7ecuqjwd5izzwhhxg4hcloagbdwxiqao2v4i7dvlsim4i/metadata.json"
-const CONTRACT_ADDRESS = "0x95F1819B6a1DEf7b9e55603986c4999495d7b267"
+import { ethers } from "ethers";
+
+const META_DATA_URL =
+  "ipfs://bafyreibue5zps7ecuqjwd5izzwhhxg4hcloagbdwxiqao2v4i7dvlsim4i/metadata.json";
+const CONTRACT_ADDRESS = "0x95F1819B6a1DEf7b9e55603986c4999495d7b267";
 //0x95F1819B6a1DEf7b9e55603986c4999495d7b267
 async function mint_NFT(contractAddress, metaDataURL) {
-   const ExampleNFT = await ethers.getContractFactory("ExampleNFT")
-   const [owner] = await ethers.getSigners()
-   await ExampleNFT.attach(contractAddress).mintNFT(owner.address, metaDataURL)
-   console.log("NFT minted to: ", owner.address)
+  const ExampleNFT = await ethers.getContractFactory("ExampleNFT");
+  const [owner] = await ethers.getSigners();
+  await ExampleNFT.attach(contractAddress).mintNFT(owner.address, metaDataURL);
+  console.log("NFT minted to: ", owner.address);
 }
 
-export async function mintNFT(){
-   try{
-      await mint_NFT(CONTRACT_ADDRESS, META_DATA_URL);
-      console.log("Asset stored successfully!");
-   }catch(error){
-      console.error(error);
-   }
+export async function mintNFT(metaData) {
+  try {
+    await mint_NFT(CONTRACT_ADDRESS, metaData);
+    console.log("Asset stored successfully!");
+  } catch (error) {
+    console.error(error);
+  }
 }
-
-
--- NFT-creation/scripts/store-asset.mjs
+++ NFT-creation/scripts/store-asset.mjs
@@ -1,29 +1,29 @@
-import { NFTStorage, File } from "nft.storage"
-import dotenv from 'dotenv'
+import { NFTStorage, File } from "nft.storage";
+import dotenv from "dotenv";
 import { deployContract } from "./deploy-contract.mjs";
-dotenv.config()
+dotenv.config();
 
-//const API_KEY = process.env.NFT_STORAGE_API_KEY;
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEIxYTcyNGU2NjA0RUUwYTQzNTg5YTdBOGYxMTBhMzNkYjg2NTcxMmQiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY5NTQ5MzYyMjY5OCwibmFtZSI6ImRiZWF0cyJ9.ctg8UzzrgpWqL539IEDqD_BLf1uZ91e939VmlDjwgDQ";
+const API_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEIxYTcyNGU2NjA0RUUwYTQzNTg5YTdBOGYxMTBhMzNkYjg2NTcxMmQiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY5NTQ5MzYyMjY5OCwibmFtZSI6ImRiZWF0cyJ9.ctg8UzzrgpWqL539IEDqD_BLf1uZ91e939VmlDjwgDQ";
 
 async function storeasset(mp3File, pngFile, title, description) {
-   const client = new NFTStorage({ token: API_KEY })
-   const metadata = await client.store({
+  const client = new NFTStorage({ token: API_KEY });
+  const metadata = await client.store({
     name: title,
     description: description,
-    image: new File([mp3File], "filename.mp3"),  // replace "filename.mp3" with an appropriate name if available
-    audio: new File([pngFile], "filename.png"),  // replace "filename.png" with an appropriate name if available
-})
-   console.log("Metadata stored on Filecoin and IPFS with URL:", metadata.url)
+    image: new File([mp3File], "filename.mp3"), // replace "filename.mp3" with an appropriate name if available
+    audio: new File([pngFile], "filename.png"), // replace "filename.png" with an appropriate name if available
+  });
+  console.log("Metadata stored on Filecoin and IPFS with URL:", metadata.url);
+  return metadata;
 }
 
-export async function storeAsset(mp3File, pngFile, title, description){
-    try{
-        await storeasset(mp3File, pngFile, title, description);
-        console.log("Asset stored successfully!");
-        await deployContract();
-    }catch(error){
-        console.error(error);
-    }
-
+export async function storeAsset(mp3File, pngFile, title, description) {
+  try {
+    const data = await storeasset(mp3File, pngFile, title, description);
+    console.log("Asset stored successfully!");
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
